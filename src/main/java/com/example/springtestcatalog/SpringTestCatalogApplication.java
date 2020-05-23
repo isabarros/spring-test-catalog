da7616593ed6c39520adf8f0f960e27e1d0cd0fe
Isadora Barros <isa.dora786@gmail.com>
2020-05-23 15:50:43 -0300

    Initial commit

---
+++ src/main/java/com/example/springtestcatalog/SpringTestCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtestcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtestcatalog/SpringTestCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtestcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
